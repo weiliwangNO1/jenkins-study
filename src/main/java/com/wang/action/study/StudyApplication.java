564bf1a85664d21a0f31fccc4e3c3daaea224159
wwl <1211840085@qq.com>
2022-05-28 21:34:03 +0800

    2022-5-28 创建test分支

--- src/main/java/com/wang/action/study/StudyApplication.java
+++ src/main/java/com/wang/action/study/StudyApplication.java
@@ -8,6 +8,7 @@ public class StudyApplication {
 
     public static void main(String[] args) {
         System.out.println("服务启动");
+        System.out.println("test分支");
         SpringApplication.run(StudyApplication.class, args);
     }
 
