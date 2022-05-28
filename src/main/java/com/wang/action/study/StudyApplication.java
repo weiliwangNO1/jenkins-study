8866000ec401dbd674835116b8dea3a33731df72
wwl <1211840085@qq.com>
2022-05-28 21:17:42 +0800

    2022-5-28 wwl第二次提交

--- src/main/java/com/wang/action/study/StudyApplication.java
+++ src/main/java/com/wang/action/study/StudyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudyApplication {
 
     public static void main(String[] args) {
+        System.out.println("服务启动");
         SpringApplication.run(StudyApplication.class, args);
     }
 
